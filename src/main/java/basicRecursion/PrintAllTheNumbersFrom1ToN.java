376ac9e4131741f060dc62dff91d97112b542675
umashav1 <vishwas.umashankar.vu1@roche.com>
2025-11-02 14:22:32 +0530

    Updated

---
+++ src/main/java/basicRecursion/PrintAllTheNumbersFrom1ToN.java
@@ -0,0 +1,32 @@
+package basicRecursion;
+
+public class PrintAllTheNumbersFrom1ToN {
+
+    public static void main(String[] args) {
+        PrintAllTheNumbersFrom1ToN obj = new PrintAllTheNumbersFrom1ToN();
+        int n = 5;
+        obj.printNumbersTailRecursion(1, n);
+        System.out.println("-----");
+        obj.printNumbersHeadRecursion(n);
+    }
+
+    public void printNumbersTailRecursion(int current, int n) {
+        // Base case
+        if (current > n) {
+            return;
+        }
+        System.out.println(current);
+        printNumbersTailRecursion(current + 1, n);
+    }
+
+    public void printNumbersHeadRecursion(int n) {
+        // Base case
+        if (n == 0) {
+            return;
+        }
+        printNumbersHeadRecursion(n - 1);
+        System.out.println(n);
+    }
+}
+
+
---
+++ src/main/java/basicRecursion/PrintValueX_NTimes.java
@@ -0,0 +1,20 @@
+package basicRecursion;
+
+public class PrintValueX_NTimes {
+
+    public static void main(String[] args) {
+        PrintValueX_NTimes obj = new PrintValueX_NTimes();
+        int x = 5;
+        int n = 3;
+        obj.printXTimes(x, n);
+    }
+
+    public void printXTimes(int x, int n) {
+        // Base case
+        if (n == 0) {
+            return;
+        }
+        System.out.println(x);
+        printXTimes(x, n - 1);
+    }
+}
